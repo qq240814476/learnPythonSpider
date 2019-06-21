4827c995b96e0d394263928857ae4d2cd0a73b06
qq240814476 <240814476@qq.com>
2019-06-21 23:43:14 +0800

    print

--- malin-spider/code_demo/baidu.py
+++ malin-spider/code_demo/baidu.py
@@ -13,3 +13,17 @@ headers = response.headers
 cookie = response.cookies
 
 print('response: %s \nhtml:%s'%(response, html))
+
+print('response: %s \nhtml:%s'%(response, content))
+
+print('response: %s \nhtml:%s'%(response, staus))
+
+print('response: %s \nhtml:%s'%(response, requestHead))
+
+print('response: %s \nhtml:%s'%(response, request))
+
+print('response: %s \nhtml:%s'%(response, headers))
+
+print('response: %s \nhtml:%s'%(response, cookie))
+
+print('response: %s \nhtml:%s'%(response, html))
