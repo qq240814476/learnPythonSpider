b676b2759f9141f79d5f562f9b7ef63c973f98d2
qq240814476 <240814476@qq.com>
2019-06-21 23:43:39 +0800

    zs

--- malin-spider/code_demo/baidu.py
+++ malin-spider/code_demo/baidu.py
@@ -12,18 +12,16 @@ request = response.request
 headers = response.headers
 cookie = response.cookies
 
-print('response: %s \nhtml:%s'%(response, html))
+# print('response: %s \nhtml:%s'%(response, html))
 
 print('response: %s \nhtml:%s'%(response, content))
 
-print('response: %s \nhtml:%s'%(response, staus))
+# print('response: %s \nhtml:%s'%(response, staus))
 
-print('response: %s \nhtml:%s'%(response, requestHead))
+# print('response: %s \nhtml:%s'%(response, requestHead))
 
-print('response: %s \nhtml:%s'%(response, request))
+# print('response: %s \nhtml:%s'%(response, request))
 
-print('response: %s \nhtml:%s'%(response, headers))
+# print('response: %s \nhtml:%s'%(response, headers))
 
-print('response: %s \nhtml:%s'%(response, cookie))
-
-print('response: %s \nhtml:%s'%(response, html))
+# print('response: %s \nhtml:%s'%(response, cookie))
