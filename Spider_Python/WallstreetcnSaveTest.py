9a04b86dfab4243ed0789cd327c97f5c75b844f6
qq240814476 <240814476@qq.com>
2019-06-22 20:47:11 +0800

    a

--- Spider_Python/WallstreetcnSaveTest.py
+++ Spider_Python/WallstreetcnSaveTest.py
@@ -34,7 +34,7 @@ def num2name(category_num):
 
 class MongoDBIO:
     # 申明相关的属性
-    def __init__(self, host, port, name, password, database, collection):
+    def __init__(self, host, port, name, password, database, collection):a
         self.host = host
         self.port = port
         self.name = name
